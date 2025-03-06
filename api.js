66919e267f6a06a9677efb0a9b583b147f91a545
Antonio Pellegrino <a.pellegrino@reply.it>
2025-03-06 13:53:39 +0100

    commmit

--- api.js
+++ api.js
@@ -1,28 +1,54 @@
-let host = "https://customfantabe.onrender.com";
+async function createUser() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+        
+    try {
 
-let apiCreaUtente = host + "/create-user";
-
-
-async function getData() {
-  try {
-        fetch(apiCreaUtente, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            title: 'Titolo esempio',
-            body: 'Contenuto del post',
-            userId: 1
-          })
+      fetch('https://customfantabe.onrender.com/create-user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: 'test',
+          mail: email,
+          password: password
         })
-      .then(response => response.json())
-      .then(data => console.log('Successo:', data))
-      .catch(error => console.error('Errore:', error));
-  } catch (error) {
-    console.error('Errore:', error);
-  }
+      })
+        .then(response => response.json())
+        .then(data => console.log('Successo:', data))
+        .catch(error => console.error('Errore:', error));
+
+
+    } catch (error) {
+        console.error("Errore:", error);
+    }
 }
 
 
-getData();
+async function makeLogin() {
+  const email = document.getElementById('email-log').value;
+  const password = document.getElementById('password-log').value;
+  
+  try {
+
+    fetch('https://customfantabe.onrender.com/make-login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: 'test',
+        mail: email,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(data => console.log('Successo:', data))
+      .catch(error => console.error('Errore:', error));
+
+
+  } catch (error) {
+      console.error("Errore:", error);
+  }
+}

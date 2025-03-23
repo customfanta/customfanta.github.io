c8ee9440e8202117bb233d8bcc003d831c1ce41e
Antonio Pellegrino <a.pellegrino@reply.it>
2025-03-23 10:08:50 +0100

    adding api to base service

--- service/api-caller.js
+++ service/api-caller.js
@@ -21,6 +21,101 @@ async function recuperaUtenteLoggato() {
     return makeGet(apiUrl);
 }
 
+async function logOut() {
+    const apiUrl = isLocal ? "../../mock/api/log-out.json" : basePath + "/logout";
+
+    return makeGet(apiUrl);
+}
+
+async function recuperaSquadra(username, chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/read-squadra.json" : basePath + "/read-squadra/${username}/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function recuperaPersonaggi(chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/read-personaggi.json" : basePath + "/read-personaggi/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function creaSquadra(nomeSquadra, descrizioneSquadra, chiaveCampionato, chiaviPersonaggi) {
+    const apiUrl = isLocal ? "../../mock/api/crea-squadra.json" : basePath + "/crea-squadra";
+
+    const body = {
+      squadra: {
+        nome: nomeSquadra,
+        descrizione: descrizioneSquadra,
+        chiaveCampionato: chiaveCampionato,
+      },
+      chiaviPersonaggi: chiaviPersonaggi,
+    };
+
+    return makePost(apiUrl, JSON.stringify(body));
+}
+
+async function invitaUtente(usernameDaInvitare, ruoloInvito, chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/invita-utente.json" : basePath + "/invita-utente";
+
+    const body = {
+      usernameUtenteInvitato: usernameDaInvitare,
+      ruoloInvito: ruoloInvito,
+      chiaveCampionato: chiaveCampionato
+    };
+
+    return makePost(apiUrl, JSON.stringify(body));
+}
+
+async function recuperaUtentiCampionato(chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/utenti-campionato.json" : basePath + "/utenti-campionato/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function rendiUtenteAdmin(username, chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/make-utente-admin.json" : basePath + "/make-utente-admin/${username}/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function rimuoviUtenteCampionato(username, chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/rimuovi-utente-campionato.json" : basePath + "/rimuovi-utente-campionato/${username}/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function recuperaAzioni(chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/read-all-azioni.json" : basePath + "/read-all-azioni/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function creaAzione(azione, descrizione, punteggio, chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/create-azione.json" : basePath + "/create-azione";
+
+    return makePost(apiUrl, JSON.stringify({ azione, descrizione, punteggio, chiaveCampionato }));
+}
+
+async function recuperaPersonaggi(chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/read-personaggi.json" : basePath + "/read-personaggi/${chiaveCampionato}";
+
+    return makeGet(apiUrl);
+}
+
+async function creaPersonaggio(nominativo, descrizione, costo, chiaveCampionato) {
+    const apiUrl = isLocal ? "../../mock/api/create-personaggio.json" : basePath + "/create-personaggio";
+
+    return makePost(apiUrl, JSON.stringify({ nominativo, descrizione, costo, chiaveCampionato }));
+}
+
+async function aggiungiAzionePersonaggio(chiaveAzione, chiavePersonaggio) {
+    const apiUrl = isLocal ? "../../mock/api/add-azione-to-personaggio.json" : basePath + "/add-azione-to-personaggio";
+
+    return makePost(apiUrl, JSON.stringify({ chiaveAzione, chiavePersonaggio }));
+}
+
+
+
 async function makeGet(apiUrl) {
     const response = await fetch(apiUrl, {
       method: "GET",
